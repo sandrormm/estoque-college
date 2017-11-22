4a922df233acb36bd0b94a6a86f151a0e51b1a2c
Igor Elias <igor.elias.muniz@gmail.com>
2017-11-21 23:38:30 -0200

    model de bairro

---
+++ models/Bairro.model.cs
@@ -0,0 +1,27 @@
+ing System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstoqueConsole
+{
+    class Bairro
+    {
+        public int cadastrarBairro(int idCidade, string nomeBairro)
+        {
+            var db = new estoqueEntities();
+
+            var bairro = db.BAIRRO.Create();
+
+            bairro.CIDADE_idCIDADE = idCidade;
+            bairro.NOME_BAIRRO = nomeBairro;
+
+            db.BAIRRO.Add(bairro);
+            db.SaveChanges();
+
+            return bairro.idBAIRRO;
+        }
+    }
+}
+
