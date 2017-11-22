e2401681edaee9093f5000160bac6cc827474275
Igor Elias <igor.elias.muniz@gmail.com>
2017-11-21 23:30:29 -0200

    model de estados

---
+++ models/Estado.model.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstoqueConsole
+{
+    class Estado: Pais
+    {
+        public int idPais { get; set; }
+        public string estado { get; set; }
+        public string uf { get; set; }
+
+        public int cadastrarEstado(int idPais, string nomeEstado, string uf = "")
+        {
+            var db = new estoqueEntities();
+
+            var estado = db.ESTADO.Create();
+
+            estado.PAIS_idPAIS = idPais;
+            estado.NOME_ESTADO = nomeEstado;
+            estado.UF = uf;
+
+            db.ESTADO.Add(estado);
+            db.SaveChanges();
+
+            return estado.idESTADO;
+        }
+    }
+}
+
